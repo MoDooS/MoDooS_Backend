e6ae2d9351044c18e34eca5d368276e9d4f070ae
syahn <496300@naver.com>
2023-06-28 16:21:16 +0900

    init: 초기 세팅

---
+++ src/main/java/com/study/modoos/ModoosApplication.java
@@ -0,0 +1,13 @@
+package com.study.modoos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModoosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModoosApplication.class, args);
+	}
+
+}
