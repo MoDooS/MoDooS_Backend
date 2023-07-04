bc54b61893d60edc76cde3a7db4d9dc3626f0a5c
syahn <496300@naver.com>
2023-07-04 14:01:08 +0900

    feat: MemberService에 회원 정보 조회, 수정 추가

---
+++ src/main/java/com/study/modoos/member/service/MemberService.java
@@ -0,0 +1,31 @@
+package com.study.modoos.member.service;
+
+import com.study.modoos.member.entity.Member;
+import com.study.modoos.member.repository.MemberRepository;
+import com.study.modoos.member.request.MemberInfoRequest;
+import com.study.modoos.member.response.MemberInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberInfoResponse getMemberInfo(Member currentMember) {
+        return memberRepository.findById(currentMember.getId())
+                .map(MemberInfoResponse::of)
+                .orElseThrow(() -> new IllegalArgumentException("유저 정보를 찾지 못했습니다."));
+    }
+
+    public void updateMemberInfo(Member currentMember, MemberInfoRequest memberInfoRequest) {
+        Long id = currentMember.getId();
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("유저 정보를 찾지 못했습니다."));
+
+        member.updateNickname(memberInfoRequest.getNickname());
+        member.updateCampus(memberInfoRequest.getCampus());
+    }
+}
