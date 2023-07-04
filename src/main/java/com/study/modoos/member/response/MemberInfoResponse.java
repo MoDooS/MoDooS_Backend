4cc5cd630cd288e2b59ae19096e823ddc567dd56
syahn <496300@naver.com>
2023-07-04 14:00:15 +0900

    feat: 마이페이지 회원 정보 response dto 생성

---
+++ src/main/java/com/study/modoos/member/response/MemberInfoResponse.java
@@ -0,0 +1,34 @@
+package com.study.modoos.member.response;
+
+import com.study.modoos.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberInfoResponse {
+
+    private String nickname;
+
+    private String email;
+
+    private String campus;
+
+    private String ranking;
+
+    private Long score;
+
+    public static MemberInfoResponse of(Member member) {
+        return MemberInfoResponse.builder()
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .campus(member.getCampus().toString())
+                .ranking(member.getRanking())
+                .score(member.getScore())
+                .build();
+    }
+}
