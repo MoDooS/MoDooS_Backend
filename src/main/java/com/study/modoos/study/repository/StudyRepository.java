bffcfb59757ee3adcf8f85da4c20baf9addca598
syahn <496300@naver.com>
2023-07-19 14:54:26 +0900

    feat: repository 수정 및 생성

---
+++ src/main/java/com/study/modoos/study/repository/StudyRepository.java
@@ -0,0 +1,9 @@
+package com.study.modoos.study.repository;
+
+import com.study.modoos.study.entity.Study;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudyRepository extends JpaRepository<Study, Long> {
+}
