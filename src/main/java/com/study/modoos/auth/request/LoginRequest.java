52d6005442bac0548a8074ac130bdbf3596595d3
syahn <496300@naver.com>
2023-07-22 01:32:12 +0900

    feat: 이메일 중복체크, 비밀번호 재설정 구현

--- src/main/java/com/study/modoos/auth/request/LoginRequest.java
+++ src/main/java/com/study/modoos/auth/request/LoginRequest.java
@@ -1,7 +1,11 @@
 package com.study.modoos.auth.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class LoginRequest {
     private String email;
--- src/main/java/com/study/modoos/member/service/MemberService.java
+++ src/main/java/com/study/modoos/member/service/MemberService.java
@@ -3,9 +3,10 @@ package com.study.modoos.member.service;
 import com.study.modoos.common.exception.ErrorCode;
 import com.study.modoos.common.exception.ModoosException;
 import com.study.modoos.member.entity.Campus;
+import com.study.modoos.member.entity.Department;
 import com.study.modoos.member.entity.Member;
 import com.study.modoos.member.repository.MemberRepository;
-import com.study.modoos.member.request.MemberInfoRequest;
+import com.study.modoos.member.request.ChangeMemberInfoRequest;
 import com.study.modoos.member.response.MemberInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,26 +22,31 @@ public class MemberService {
                 .orElseThrow(() -> new ModoosException(ErrorCode.MEMBER_NOT_FOUND));
     }
 
-    public void updateMemberInfo(Member currentMember, MemberInfoRequest memberInfoRequest) {
-        try {
-            currentMember.updateNickname(memberInfoRequest.getNickname());
-            String campus = memberInfoRequest.getCampus();
-            currentMember.updateCampus(Campus.valueOf(campus));
-            memberRepository.save(currentMember);
-        } catch (IllegalArgumentException e) {
+    public void updateMemberInfo(Member currentMember, ChangeMemberInfoRequest memberInfoRequest) {
+        currentMember.updateNickname(memberInfoRequest.getNickname());
+        Campus campus = memberInfoRequest.getCampus();
+        Department department = memberInfoRequest.getDepartment();
+
+        if (!campus.hasDepartment(department))
             throw new ModoosException(ErrorCode.VALUE_NOT_IN_OPTION);
-        }
+        currentMember.updateCampus(campus);
+        currentMember.updateDepartment(department);
+        memberRepository.save(currentMember);
     }
 
     public void join(Member member) {
         if (nicknameCheck(member.getNickname())) {
-            throw new ModoosException(ErrorCode.FORBIDDEN_ARTICLE);
+            throw new ModoosException(ErrorCode.ALREADY_MEMBER);
         }
         memberRepository.save(member);
     }
 
     public boolean nicknameCheck(String nickname) {
         return memberRepository.findByNickname(nickname).isPresent();
+    }
+
+    public boolean emailCheck(String email) {
+        return memberRepository.findByEmail(email).isPresent();
     }
 
     public Member findByEmail(String email) {
