6f258161fe6a83694bce1f773181ce7735e83ff5
tk.yoon <tk.yoon0105@gmail.com>
2023-07-29 21:05:12 +0900

    fix: health-check api 수정

--- src/main/java/com/study/modoos/HealthCheckController.java
+++ src/main/java/com/study/modoos/HealthCheckController.java
@@ -1,15 +1,13 @@
 package com.study.modoos;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("api/health-check")
 public class HealthCheckController {
-    @GetMapping("/health-check")
+    @GetMapping
     public Long heathCheck() {
         return System.currentTimeMillis();
     }
