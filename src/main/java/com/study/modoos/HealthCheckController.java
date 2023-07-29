3189dddf72838e09bf6833a3c441bf8cafbdf0da
tk.yoon <tk.yoon0105@gmail.com>
2023-07-29 15:05:03 +0900

    fix: health-check 에러 수정

--- src/main/java/com/study/modoos/HealthCheckController.java
+++ src/main/java/com/study/modoos/HealthCheckController.java
@@ -1,11 +1,15 @@
 package com.study.modoos;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/health-check")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class HealthCheckController {
-    @GetMapping
+    @GetMapping("/health-check")
     public Long heathCheck() {
         return System.currentTimeMillis();
     }
