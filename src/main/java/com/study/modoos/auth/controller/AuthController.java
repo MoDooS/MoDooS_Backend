d3e8ba3ba77f7fd6fc0ababda1fb75ec048c7273
tk.yoon <tk.yoon0105@gmail.com>
2023-07-31 17:40:10 +0900

    fix: api 에러 수정

--- src/main/java/com/study/modoos/auth/controller/AuthController.java
+++ src/main/java/com/study/modoos/auth/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("api/auth")
 public class AuthController {
 
     private final AuthService authService;
