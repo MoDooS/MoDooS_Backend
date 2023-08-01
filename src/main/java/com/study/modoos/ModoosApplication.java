2547f8fc556793c1a79dd14be9bde3025e39af64
syahn <496300@naver.com>
2023-08-01 17:08:57 +0900

    chore: queryDsl 설정

--- src/main/java/com/study/modoos/ModoosApplication.java
+++ src/main/java/com/study/modoos/ModoosApplication.java
@@ -2,12 +2,14 @@ package com.study.modoos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ModoosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModoosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ModoosApplication.class, args);
+    }
 
 }
