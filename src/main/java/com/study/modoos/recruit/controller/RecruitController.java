63f24874a615f5053414e644db5aa739247ab984
syahn <496300@naver.com>
2023-08-01 17:11:15 +0900

    feat: 검색어, 정렬순, 카테고리 필터링 기반 모집글 공고 조회 구현

--- src/main/java/com/study/modoos/recruit/controller/RecruitController.java
+++ src/main/java/com/study/modoos/recruit/controller/RecruitController.java
@@ -5,11 +5,19 @@ import com.study.modoos.common.response.NormalResponse;
 import com.study.modoos.member.entity.Member;
 import com.study.modoos.recruit.request.ChangeRecruitRequest;
 import com.study.modoos.recruit.request.RecruitRequest;
-import com.study.modoos.recruit.response.RecruitResponse;
+import com.study.modoos.recruit.response.RecruitIdResponse;
+import com.study.modoos.recruit.response.RecruitInfoResponse;
 import com.study.modoos.recruit.service.RecruitService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,12 +26,27 @@ public class RecruitController {
     private final RecruitService recruitService;
 
     @PostMapping("/post")
-    public ResponseEntity<RecruitResponse> createRecruit(@CurrentUser Member member, @RequestBody RecruitRequest recruitRequest) {
+    public ResponseEntity<RecruitIdResponse> createRecruit(@CurrentUser Member member, @RequestBody RecruitRequest recruitRequest) {
         return ResponseEntity.ok(recruitService.postRecruit(member, recruitRequest));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<RecruitInfoResponse> getRecruit(@CurrentUser Member member, @PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(recruitService.oneRecruit(member, id));
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<Slice<RecruitInfoResponse>> getRecruitList(@CurrentUser Member member,
+                                                                     @RequestParam(value = "category", defaultValue = "") List<String> category,
+                                                                     @RequestParam(value = "searchBy", required = false) String search,
+                                                                     @PageableDefault(sort = "createdAt", direction = DESC) Pageable pageable) {
+
+        return ResponseEntity.ok(recruitService.getRecruitList(member, search, category, pageable));
+
+    }
+
     @PutMapping("/{id}")
-    public ResponseEntity<RecruitResponse> changeRecruit(@CurrentUser Member member, @PathVariable(value = "id") Long id, @RequestBody ChangeRecruitRequest request) {
+    public ResponseEntity<RecruitIdResponse> changeRecruit(@CurrentUser Member member, @PathVariable(value = "id") Long id, @RequestBody ChangeRecruitRequest request) {
         return ResponseEntity.ok(recruitService.changeRecruit(member, id, request));
     }
 
