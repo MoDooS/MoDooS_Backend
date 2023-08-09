74dfdd5c804ad926e03b9c6e4443eadfb91b791c
tk.yoon <tk.yoon0105@gmail.com>
2023-08-09 14:33:51 +0900

    fix: cors Origin url 추가

--- src/main/java/com/study/modoos/common/config/CorsConfig.java
+++ src/main/java/com/study/modoos/common/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
+        config.addAllowedOriginPattern("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
