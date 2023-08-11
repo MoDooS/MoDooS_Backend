3036d5d52498b533f84d7a556e98d09eb9eb3151
syahn <496300@naver.com>
2023-08-11 17:41:58 +0900

    refactor: 참여 신청 관련 로직 수정

--- src/main/java/com/study/modoos/participant/controller/ParticipantController.java
+++ src/main/java/com/study/modoos/participant/controller/ParticipantController.java
@@ -8,7 +8,10 @@ import com.study.modoos.participant.response.StandbyResponse;
 import com.study.modoos.participant.service.ParticipantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,18 +21,18 @@ public class ParticipantController {
     private final ParticipantService participantService;
 
     @GetMapping("/apply/{studyId}")
-    public ResponseEntity<StandbyResponse> applyStudy (@CurrentUser Member member,
-                                                       @PathVariable(value = "studyId") Long studyId){
+    public ResponseEntity<StandbyResponse> applyStudy(@CurrentUser Member member,
+                                                      @PathVariable(value = "studyId") Long studyId) {
         return ResponseEntity.ok(participantService.applyStudy(member, studyId));
     }
 
     @GetMapping("/accept/{standbyId}")
-    public ResponseEntity<ParticipantResponse> acceptApplication (@CurrentUser Member member, @PathVariable Long standbyId) {
+    public ResponseEntity<ParticipantResponse> acceptApplication(@CurrentUser Member member, @PathVariable(value = "standbyId") Long standbyId) {
         return ResponseEntity.ok(participantService.acceptApplication(member, standbyId));
     }
 
     @GetMapping("/reject/{standbyId}")
-    public ResponseEntity<NormalResponse> rejectApplication(@CurrentUser Member member, @PathVariable Long standbyId) {
+    public ResponseEntity<NormalResponse> rejectApplication(@CurrentUser Member member, @PathVariable(value = "standbyId") Long standbyId) {
         participantService.rejectApplication(member, standbyId);
         return ResponseEntity.ok(NormalResponse.success());
     }
