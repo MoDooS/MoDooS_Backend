49aafb1d804c856f960626740a04391923a8a26c
tk.yoon <tk.yoon0105@gmail.com>
2023-08-18 01:01:24 +0900

    feat: 전체 랭킹 조회 구현

---
+++ src/main/java/com/study/modoos/ranking/service/RankingService.java
@@ -0,0 +1,49 @@
+package com.study.modoos.ranking.service;
+
+import com.study.modoos.common.exception.ErrorCode;
+import com.study.modoos.common.exception.ModoosException;
+import com.study.modoos.member.entity.Member;
+import com.study.modoos.member.repository.MemberRepository;
+import com.study.modoos.ranking.response.RankingResponse;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RankingService {
+    private final MemberRepository memberRepository;
+
+    public RankingService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Slice<RankingResponse> getRanking(Member currentUser, Pageable pageable) {
+        Member member = memberRepository.findByEmail(currentUser.getEmail())
+                .orElseThrow(() -> new ModoosException(ErrorCode.MEMBER_NOT_FOUND));
+
+        Pageable sortedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(Sort.Direction.DESC, "score"));
+
+        Slice<Member> rankedMembersSlice = memberRepository.findAll(sortedPageable);
+
+        List<RankingResponse> rankingList = rankedMembersSlice.stream()
+                .map(RankingResponse::of)
+                .collect(Collectors.toList());
+
+
+        int currentRank = rankedMembersSlice.getNumber() * rankedMembersSlice.getSize() + 1;
+        long currentScore = rankingList.isEmpty() ? -1 : rankingList.get(0).getScore();
+
+        for (RankingResponse response : rankingList) {
+            if (response.getScore() < currentScore) {
+                currentRank++;
+                currentScore = response.getScore();
+            }
+            response.setRankingNumber((long) currentRank);
+        }
+
+        return new SliceImpl<>(rankingList, sortedPageable, rankedMembersSlice.hasNext());
+    }
+    }
+
