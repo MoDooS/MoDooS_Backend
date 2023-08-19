6c73a743c9e59908e190430eb56f37a83f1f3768
tk.yoon <tk.yoon0105@gmail.com>
2023-08-19 17:00:50 +0900

    feat: 알람 엔티티 생성

---
+++ src/main/java/com/study/modoos/alarm/entity/Alarm.java
@@ -0,0 +1,46 @@
+package com.study.modoos.alarm.entity;
+
+import com.study.modoos.comment.entity.Comment;
+import com.study.modoos.common.entity.BaseTimeEntity;
+import com.study.modoos.member.entity.Member;
+import com.study.modoos.study.entity.Study;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "Alarm")
+public class Alarm extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", referencedColumnName = "id")
+    private Member member;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id", referencedColumnName = "id")
+    private Study study;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", referencedColumnName = "id")
+    private Comment comment;
+
+    @Enumerated(EnumType.STRING)
+    private AlarmType alarmType;
+    private boolean isRead;
+
+    @Builder
+    public Alarm(Member member, Study study, Comment comment, AlarmType alarmType) {
+        this.member = member;
+        this.study =study;
+        this.comment = comment;
+        this.alarmType = alarmType;
+    }
+}
