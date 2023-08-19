6e168aff8a4ae60b792cb2265efa037b6249bdb5
tk.yoon <tk.yoon0105@gmail.com>
2023-08-19 17:01:20 +0900

    feat: 알람 api 기능 구현

---
+++ src/main/java/com/study/modoos/alarm/response/AlarmResponse.java
@@ -0,0 +1,33 @@
+package com.study.modoos.alarm.response;
+
+import com.study.modoos.alarm.entity.Alarm;
+import com.study.modoos.alarm.entity.AlarmType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AlarmResponse {
+    private Long alarmId;
+    private String receiverName;
+    private Long receiverId;
+    private String studyName;
+    private Long studyId;
+    private String content;
+    private Long commentId;
+    private AlarmType alarmType;
+
+    public static AlarmResponse of(Alarm alarm) {
+        AlarmResponse response = new AlarmResponse();
+        response.alarmId = alarm.getId();
+        response.receiverName = alarm.getMember().getNickname();
+        response.receiverId = alarm.getMember().getId();
+        response.studyName = alarm.getStudy().getTitle();
+        response.studyId = alarm.getStudy().getId();
+        response.content = alarm.getContent();
+        response.commentId = alarm.getComment().getId();
+        response.alarmType = alarm.getAlarmType();
+        return response;
+    }
+}
---
+++ src/main/java/com/study/modoos/alarm/service/AlarmService.java
@@ -0,0 +1,40 @@
+package com.study.modoos.alarm.service;
+
+import com.study.modoos.alarm.entity.Alarm;
+import com.study.modoos.alarm.repository.AlarmRepository;
+import com.study.modoos.alarm.response.AlarmResponse;
+import com.study.modoos.common.exception.ErrorCode;
+import com.study.modoos.common.exception.ModoosException;
+import com.study.modoos.member.entity.Member;
+import com.study.modoos.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AlarmService {
+    private final AlarmRepository alarmRepository;
+    private final MemberRepository memberRepository;
+
+    public Slice<AlarmResponse> getAlarm(Member currentUser, Pageable pageable) {
+        Member member = memberRepository.findByEmail(currentUser.getEmail())
+                .orElseThrow(() -> new ModoosException(ErrorCode.MEMBER_NOT_FOUND));
+
+        Pageable sortedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        Slice<Alarm> alarmSlice = alarmRepository.findByMemberOrderByCreatedAtDesc(member, sortedPageable);
+
+        List<AlarmResponse> alarmResponses = alarmSlice.getContent().stream()
+                .map(AlarmResponse::of)
+                .collect(Collectors.toList());
+
+        return new SliceImpl<>(alarmResponses, sortedPageable, alarmSlice.hasNext());
+
+    }
+}
