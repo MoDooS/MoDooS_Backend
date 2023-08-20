9249cf3b4188d95470a7c4a63cd29beb3393aa34
tk.yoon <tk.yoon0105@gmail.com>
2023-08-20 18:11:33 +0900

    feat: 찜한 스터디 모집 마감일 하루 전 알림 추가 구현

--- src/main/java/com/study/modoos/heart/repository/HeartRepository.java
+++ src/main/java/com/study/modoos/heart/repository/HeartRepository.java
@@ -5,7 +5,17 @@ import com.study.modoos.heart.entity.Heart;
 import com.study.modoos.member.entity.Member;
 import com.study.modoos.study.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
     Heart findByMemberAndStudy(Member currentUser, Study study);
+
+    @Query("SELECT h.study FROM Heart h WHERE h.study.status = 'RECRUITING' AND h.study.recruit_deadline = ?1")
+    List<Study> findStudiesByRecruitDeadline(LocalDate recruitDeadline);
+
+    @Query("SELECT h.member FROM Heart h WHERE h.study = ?1")
+    List<Member> findMembersByStudy(Study study);
 }
