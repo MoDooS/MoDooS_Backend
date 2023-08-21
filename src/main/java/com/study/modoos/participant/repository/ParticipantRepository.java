397887832ca5a9df709c8636e73baa2d6645b192
tk.yoon <tk.yoon0105@gmail.com>
2023-08-21 19:53:38 +0900

    feat: 피드백 시작, 피드백 마감 2시간 전 알림 추가

--- src/main/java/com/study/modoos/participant/repository/ParticipantRepository.java
+++ src/main/java/com/study/modoos/participant/repository/ParticipantRepository.java
@@ -4,6 +4,7 @@ import com.study.modoos.member.entity.Member;
 import com.study.modoos.participant.entity.Participant;
 import com.study.modoos.study.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,4 +19,7 @@ public interface ParticipantRepository extends JpaRepository<Participant, Long> 
     List<Participant> findByStudy(Study study);
 
     List<Participant> findByMember(Member member);
+
+    @Query("SELECT p.study FROM Participant p WHERE p.study.status = 'ONGOING' AND p.member = ?1")
+    List<Study> findOngoingStudyByMember(Member member);
 }
