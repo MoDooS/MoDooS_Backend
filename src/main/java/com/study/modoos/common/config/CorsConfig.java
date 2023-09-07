46710790c8edb06afe2b6754f03e46db06d1562a
tk.yoon <126501685+YOONTAEKYUN@users.noreply.github.com>
2023-09-07 14:56:55 +0900

    cors 에러 수정(도메인 Origin 추가)
    www.modustudy.com, modustudy.com 도메인 corsOrigin 추가

--- src/main/java/com/study/modoos/common/config/CorsConfig.java
+++ src/main/java/com/study/modoos/common/config/CorsConfig.java
@@ -14,6 +14,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("https://modoos.vercel.app");
+        config.addAllowedOrigin("https://www.modustudy.com");
+        config.addAllowedOrigin("https://modustudy.com");
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addExposedHeader("Set-Cookie");
